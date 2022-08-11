33bba0565fdf3e36b2687df4e4b47ab2aa0bbb4b
Muhammed Yasin BEKTAŞ <bektasyasin707@gmail.com>
2022-08-11 18:55:11 +0300

    import-1

---
+++ todo.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/bekta/Desktop/Kodlama Egzersizleri/VS code/TodoApp/todo.db'
+db = SQLAlchemy(app)
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
+
+
